3505c5a5bf4c24142cec15661e3b7fcb4aa5f176
sawungrana <azzaravi@gmail.com>
2021-06-06 14:04:59 +0700

    "new commit"

--- app.js
+++ app.js
@@ -2,4 +2,19 @@ const express = require('express');
 
 const app = express();
 
+//middleware
+app.use('/posts', () => {
+    console.log('Middleware sedang berjalan');
+})
+
 //iso gawe route
+app.get('/', (req, res) => {
+    res.send('JAAAAAANCCOOOOOOOOOOOOOO');
+})
+
+app.get('/posts', (req, res) => {
+    res.send('Neng posts cuk');
+})
+
+//server
+app.listen(3000);
